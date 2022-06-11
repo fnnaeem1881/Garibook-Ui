e40983aacd3daa15e717aedb4f0de7b03cacd7fd
fnnaeem1881 <mehedinaeem66@gmail.com>
2022-06-11 15:51:41 +0600

    form design complete

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -576,12 +576,51 @@ $(document).on('ready', function() {
         infinite: false,
         slidesToShow: 3,
         slidesToScroll: 1,
-        variableWidth: true
+        variableWidth: true,
+        autoplay: true,
+
     });   
     $(".services_box_slide").slick({
         infinite: false,
         slidesToShow: 5,
         slidesToScroll: 1,
+        variableWidth: false,
+        autoplay: true,
+        responsive: [
+            {
+              breakpoint: 1200,
+              settings: {
+                slidesToShow: 4,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: true
+              }
+            },
+            {
+              breakpoint: 991,
+              settings: {
+                slidesToShow: 3,
+                slidesToScroll: 2
+              }
+            },
+            {
+              breakpoint: 768,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+              }
+            },
+            {
+              breakpoint: 480,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1
+              }
+            }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+          ]
     });
 });
 
